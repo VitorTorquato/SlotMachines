6c8cdafe44384da89ed2ef8d904e82558d52af1b
Vitor Torquato de Sousa <torquatovitor68@gmail.com>
2025-01-28 11:40:34 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'slot-machine-list.html', // Primeiro arquivo
-          dest: '' // Copiar para a raiz do dist
-        },
-        {
-          src: 'selected-slot-machine.html', // Segundo arquivo
-          dest: '' // Copiar também para a raiz do dist
-        },
-      ],
-    }),
-  ],
-  build: {
-    outDir: 'dist', // Pasta de saída
-  },
-});
+  plugins: [react()],
+})
